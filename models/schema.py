bfbe8dffac94c295d131b2b138874cc9ac939239
RyamL1221 <ryaml1221@gmail.com>
2025-03-02 22:53:42 -0500

    changed schema and removed extra line

--- models/schema.py
+++ models/schema.py
@@ -3,4 +3,3 @@ from marshmallow import Schema, fields
 class DataSchema(Schema):
     auth_token = fields.String(required=True)
     email = fields.Email(required=True)
-    count = fields.Integer(required=True)
