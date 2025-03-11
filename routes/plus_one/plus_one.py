95815266a359e86e52eeffe6a55ef5c135d819b3
RyamL1221 <ryaml1221@gmail.com>
2025-03-10 23:29:28 -0400

    changed imports for schema

--- routes/plus_one/plus_one.py
+++ routes/plus_one/plus_one.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from marshmallow import ValidationError
 from bson import ObjectId
-from models.schema import DataSchema
+from schema import DataSchema
 from database.MongoDB import MongoDB
 
 plus_one_bp = Blueprint("plus_one", __name__)
