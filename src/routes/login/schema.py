121cfa8b560db7083578f0a459d40bba9e9e067c
RyamL1221 <ryaml1221@gmail.com>
2025-03-26 18:51:48 -0400

    fixed login endpoint. removed auth_token in schema

--- src/routes/login/schema.py
+++ src/routes/login/schema.py
@@ -1,6 +1,5 @@
 from marshmallow import Schema, fields
 
 class DataSchema(Schema):
-    auth_token = fields.String(required=True)
     email = fields.String(required=True)
     password = fields.String(required=True)
