b66940bbb05b7121f3655c6ec23a04ebe1456eb1
RyamL1221 <ryaml1221@gmail.com>
2025-05-12 08:34:21 -0400

    completed forgot password endpoint

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -5,5 +5,6 @@ from .read import read_bp
 from .register import register_bp
 from .login import login_bp
 from .get_security_question import get_security_question_bp
+from .forgot_password import forgot_password_bp
 
-__all__ = ["default_bp", "plus_one_bp", "minus_one_bp", "read_bp", "register_bp", "login_bp", "get_security_question_bp"]
+__all__ = ["default_bp", "plus_one_bp", "minus_one_bp", "read_bp", "register_bp", "login_bp", "get_security_question_bp", "forgot_password_bp"]
