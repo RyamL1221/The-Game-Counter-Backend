c5d5d2db5879851f4de77c4ca24b1d0712f5b42e
RyamL1221 <ryaml1221@gmail.com>
2025-05-11 22:02:24 -0400

    changed register endpoint schema to include security question

--- src/routes/register/schema.py
+++ src/routes/register/schema.py
@@ -3,3 +3,5 @@ from marshmallow import Schema, fields
 class DataSchema(Schema):
     email = fields.String(required=True)
     password = fields.String(required=True)
+    security_question = fields.String(required=True)
+    security_answer = fields.String(required=True)
