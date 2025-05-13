2c92a822b12560707e5128b23a1cb149eef1b9d4
RyamL1221 <ryaml1221@gmail.com>
2025-05-13 08:28:24 -0400

    added data-analysis endpoint to main

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from src.routes import default_bp, plus_one_bp, minus_one_bp, read_bp, register_bp, login_bp, get_security_question_bp, forgot_password_bp
+from src.routes import default_bp, plus_one_bp, minus_one_bp, read_bp, register_bp, login_bp, get_security_question_bp, forgot_password_bp, data_analysis_bp
 
 def create_app():
     app = Flask(__name__)
@@ -15,6 +15,7 @@ def create_app():
     app.register_blueprint(login_bp)
     app.register_blueprint(get_security_question_bp)
     app.register_blueprint(forgot_password_bp)
+    app.register_blueprint(data_analysis_bp)
     
     return app
 
